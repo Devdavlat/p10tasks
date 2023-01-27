f215e3f52820f1bcc4d29d5717b02662b92e9a4b
Devdavlat <davlatbekm004@gmail.com>
2023-01-27 16:46:47 +0500

    fixed errors

--- generator_iter_decorator/task_3.py
+++ generator_iter_decorator/task_3.py
@@ -5,14 +5,16 @@ bunda funksiya qiymati 2 ga ko'payirilgan holda qaytarilsin.
 """
 
 
-
-def main_funct():
-    def extra_func(n, m):
-        return (n + m) * 2
+def main_funct(funct):
+    def extra_func(a, b):
+        return funct(a * 2, b * 2)
 
     return extra_func
 
 
-marge = main_funct()
+@main_funct
+def marge(a, b):
+    return a + b
+
 
 print(marge(10, 3))
